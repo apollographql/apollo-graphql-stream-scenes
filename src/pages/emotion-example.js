da2ce81faf54986593647b973713b21f9114976e
Kurt Kemple <kurtkemple@kurtsapollombp.lan>
2020-05-05 14:28:47 -0400

    initial commit :sparkle:

---
+++ src/pages/emotion-example.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default () => (
+  <div
+    css={{
+      backgroundColor: 'hotpink',
+      '&:hover': {
+        color: 'lightgreen'
+      }
+    }}
+  >
+    This has a hotpink background.
+  </div>
+)
