ee7e2fa9ca6a3a67cbc70d90c59caccc455f7cf5
Kurt Kemple <kurtkemple@kurtsapollombp.lan>
2020-05-05 14:45:57 -0400

    testing background colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function IndexPage() {
   return (
-    <div css={{ width: "100vw", height: "100vh", backgroundColor: "white" }}>
+    <div css={{ width: "100vw", height: "100vh", backgroundColor: "#010101" }}>
       <h1>New Site!</h1>
     </div>
   );
