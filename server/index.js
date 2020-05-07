ea6b3452654eca6704f5c8f74d5a737eac532b34
Kurt Kemple <kurtiskemple@gmail.com>
2020-05-07 17:40:55 -0400

    add playground

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
